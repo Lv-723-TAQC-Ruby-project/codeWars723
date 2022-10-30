a1b46befd444430e6847084e9b43b2ca4dc9d093
bohdan-khudio <bahdan510@gmail.com>
2022-10-30 17:49:12 +0200

    add my task

---
+++ Bohdan Khudo/Wilson primes.rb
@@ -0,0 +1,30 @@
+def am_I_Wilson(p)
+  if p == 5 || p == 13 || p == 563
+    return true
+  else
+    return false
+  end
+end
+# second way
+# def am_I_Wilson(p)
+#   if p == 0
+#     return false
+#   else
+#     res = (factorial(p - 1) + 1.0) / (p * p)
+#     if res % 1 == 0
+#       return true
+#     else
+#       return false
+#     end
+#   end
+# end
+#
+# def factorial(x)
+#   f = 1;
+#   for i in 1..x
+#     f *= i
+#   end
+#   return f
+# end
+#
+# puts am_I_Wilson(5)
--- Bohdan Khudo/bohdan_khudo.rb
+++ Bohdan Khudo/bohdan_khudo.rb
@@ -1,17 +1,20 @@
 require './Bohdan Khudo/Keep Hydrated!'
+require './Bohdan Khudo/Volume'
 
 class BohdanKhudo
   def task1
     puts 'Input time:'
-    time = gets.chomp
-    puts "result = #{litres(time.to_i)}"
+    time = gets.chomp.to_i
+    puts "result = #{litres(time)}"
   end
 
   def task2
+    puts 'Input time: length, width and height'
+    length = gets.chomp.to_i
+    width = gets.chomp.to_i
+    height = gets.chomp.to_i
+    puts "result = #{get_volume_of_cuboid(length, width, height)}"
 
   end
 
-  def task3
-
-  end
 end
