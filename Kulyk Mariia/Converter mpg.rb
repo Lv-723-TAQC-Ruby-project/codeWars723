5669f6138a80244074cafd060a364c3e0ce3f568
merykul <mariakulv@gmail.com>
2022-10-30 18:00:49 +0300

    initial commit of tasks

---
+++ Kulyk Mariia/Converter mpg.rb
@@ -0,0 +1,8 @@
+puts "Enter miles per imperial gallon: "
+
+def converter(mpg)
+  output = (mpg * 1.609344/4.54609188).round(2)
+  return output
+end
+
+puts converter(gets.to_f);
---
+++ Kulyk Mariia/Keep Hydrated.rb
@@ -0,0 +1,7 @@
+puts "How long you would like cycling?"
+
+def litres(time)
+  (time * 0.5).floor
+end
+
+puts litres(gets.to_f)
