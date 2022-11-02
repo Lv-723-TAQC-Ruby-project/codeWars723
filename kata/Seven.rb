ed31a26cd87bc00b2bded7dccfa8b7040d3b2ecf
Coffezka <42944711+Coffezka@users.noreply.github.com>
2022-11-02 18:24:35 +0200

    Add comments and create abstract class Six

--- kata/Seven.rb
+++ kata/Seven.rb
@@ -1,12 +1,16 @@
 class Seven
+
+    #Looking for a benefactor
     def new_avg(arr, newavg)
         raise NotImplementedError, "text"
     end
 
+    #Sum of the first nth term of Series
     def series_sum(n)
         raise NotImplementedError, "text"
     end
 
+    #Where is Vasya?
     def where_is_he(p, bef, aft)
         raise NotImplementedError, "text"
     end
