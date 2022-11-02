22efc5d9f1a794cd056129f266e740ddb4eec918
Coffezka <42944711+Coffezka@users.noreply.github.com>
2022-11-02 18:11:16 +0200

    modified class Eight

--- kata/Eight.rb
+++ kata/Eight.rb
@@ -6,4 +6,33 @@ class Eight
     def get_volume_of_cuboid(length, width, height)
         raise NotImplementedError, "text" 
     end
+
+    def converter(mpg)
+        raise NotImplementedError, "text" 
+    end
+
+    def square_or_square_root(arr)
+        raise NotImplementedError, "text"
+    end
+
+    def count_positives_sum_negatives(lst)
+        raise NotImplementedError, "text"
+    end
+
+    def string_to_number(s)
+        raise NotImplementedError, "text"
+    end
+
+    def am_I_Wilson(p)
+        raise NotImplementedError, "text"
+    end
+
+    def two_decimal_places(n)
+        raise NotImplementedError, "text"
+    end
+
+    def divisible_by(numbers, divisor)
+        raise NotImplementedError, "text"
+    end
+
 end
