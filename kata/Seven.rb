236b4bf43bcf8987868272ac1da0ef4542c89e61
Coffezka <42944711+Coffezka@users.noreply.github.com>
2022-11-02 18:15:55 +0200

    Create abstract class Seven

---
+++ kata/Seven.rb
@@ -0,0 +1,13 @@
+class Seven
+    def new_avg(arr, newavg)
+        raise NotImplementedError, "text"
+    end
+
+    def series_sum(n)
+        raise NotImplementedError, "text"
+    end
+
+    def where_is_he(p, bef, aft)
+        raise NotImplementedError, "text"
+    end
+end
