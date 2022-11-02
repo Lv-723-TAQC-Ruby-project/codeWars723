669ce85a8a673d61dc32c3b8b90a422bccca35cb
Coffezka <42944711+Coffezka@users.noreply.github.com>
2022-11-02 18:55:08 +0200

    Modified class ConsoleReader

--- utils/ConsoleReader.rb
+++ utils/ConsoleReader.rb
@@ -1,5 +1,31 @@
 class ConsoleReader
+
     def read_int
+        while true
+            temp = gets.chomp
+            begin
+                temp = Integer(temp)
+                return temp
+              rescue ArgumentError, TypeError
+                puts "Invalid input, please try again:"
+              end
+        end
+    end
+
+    def read_float
+        while true
+            temp = gets.chomp
+            begin
+                temp = Float(temp)
+                return temp
+              rescue ArgumentError, TypeError
+                puts "Invalid input, please try again:"
+              end
+        end
+    end
+
+    def read_string
         #ToDo
     end
+
 end
