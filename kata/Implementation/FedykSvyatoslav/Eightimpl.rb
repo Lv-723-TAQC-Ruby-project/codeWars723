8c9e78e9ab01ed9e62b9a056968a1b86b39507bc
Coffezka <42944711+Coffezka@users.noreply.github.com>
2022-11-02 20:48:37 +0200

    Instanse methods

--- kata/Implementation/FedykSvyatoslav/Eightimpl.rb
+++ kata/Implementation/FedykSvyatoslav/Eightimpl.rb
@@ -1,10 +1,10 @@
-require "../Eight.rb"
+require "./kata/Eight.rb"
 
 class EightImpl<Eight
-    def litres(time)
+    def self.litres(time)
         return (time * 0.5).floor
     end
-    def get_volume_of_cuboid(length, width, height)
+    def self.get_volume_of_cuboid(length, width, height)
         #ToDo
     end
 end
--- utils/TaskRuner.rb
+++ utils/TaskRuner.rb
@@ -1,16 +1,34 @@
+require './utils/ConsoleReader.rb'
+
 class TaskRuner
-    @@user = "A"
+    @@user = ""
     @@reader = ConsoleReader.new
+
+    def litres
+        require "./kata/Implementation/#{@@user}/EightImpl.rb"
+        puts "time:"
+        time = @@reader.read_int   
+        result = EightImpl.litres(time)
+        puts "Result : #{result}"
+    end
 
     def run_get_volume_of_cuboid
         puts "length:"
-        length = reader.read_int
+        length = @@reader.read_int
         puts "width:"
-        width  = reader.read_int
+        width  = @@reader.read_int
         puts "height:"
-        height = @reader.read_int
-        #user.k8.get_volume_of_cuboid
-        result = EightImpl.get_volume_of_cuboid(length, width, height)
+        height = @@reader.read_int
+        #result = EightImpl.get_volume_of_cuboid(length, width, height)
         puts result
     end
+
+    def set_user(user) 
+        @@user = user 
+    end
+
+    def get_user
+        @@user
+    end
 end
+
