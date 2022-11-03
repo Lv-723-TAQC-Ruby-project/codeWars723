e9cbd991e6ad827bb4e81a26f65e8d47138747ac
sofiy <sofiya.ometiukh@gmail.com>
2022-11-03 13:08:13 +0200

    Added class Sevenlmpl to SofiiaOmetiukh Implementation

--- kata/Implementation/SofiiaOmetiukh/Sevenlmpl.rb
+++ kata/Implementation/SofiiaOmetiukh/Sevenlmpl.rb
@@ -1,4 +1,24 @@
 
 class SevenImpl<Seven
 
+  def self.new_avg(arr, newavg)
+    x = (newavg * (arr.length + 1) - arr.sum).ceil
+    raise 'Inappropriate donation amount' if(x <= 0)
+    return x
+  end
+  def self.series_sum(n)
+    number = 1
+    res = 0
+    t = 0
+    while t < n
+      res += 1.0/number
+      number +=3
+      t += 1
+    end
+    format("%.2f", res)
+  end
+
+  def self.where_is_he(p, bef, aft)
+    p - [bef + 1, p - aft].max + 1
+  end
 end
