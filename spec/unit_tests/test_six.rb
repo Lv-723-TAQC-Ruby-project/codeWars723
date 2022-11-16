5efa1bd8f09966bcca911a0576b2440905eb98a4
Coffezka <42944711+Coffezka@users.noreply.github.com>
2022-11-16 15:30:05 +0200

    Save

--- spec/unit_tests/test_six.rb
+++ spec/unit_tests/test_six.rb
@@ -1,1 +1,14 @@
 
+require "./utils/ListImpl.rb"
+require 'rspec'
+
+ListImpl::IMPL.each do |student_name, student_impl|
+    student_impl = student_impl::SixImpl
+
+    RSpec.describe student_impl do
+        describe '.balance' do
+           
+        end
+        
+    end
+end
