a4698aa6f9f4527831d9fbc464f99156425f2727
Coffezka <42944711+Coffezka@users.noreply.github.com>
2022-11-16 19:13:46 +0200

    Minor fix and add test for method balance

--- kata/Implementation/FedykSvyatoslav/SixImpl.rb
+++ kata/Implementation/FedykSvyatoslav/SixImpl.rb
@@ -19,28 +19,32 @@ module FedykSvyatoslavSixImpl
     def self.balance(b)
       sym = "&@,;$#€!=:;-_+^/(){}?*'%"
       new_b = b.dup
-
-      # clean text
+      
+      #clean text
       new_b.gsub!(/[^0-9A-Za-z.\r\n]/, ' ')
       arr = new_b.split("\n")
-      arr = arr.reject(&:empty?)
+      arr = arr.reject { |elem| elem.strip.empty?}
+      
+      puts arr
 
       full_balance = arr[0].to_f
       balance = arr[0].to_f
-
-      arr[0] = format('Original Balance: %0.2f', arr[0].to_f)
-
-      (1..(arr.length - 1)).each do |k|
-        n = arr[k].split(' ')
-        balance -= n[2].to_f
-        arr[k] = format('%s %s %0.2f Balance %0.2f', n[0], n[1], n[2].to_f, balance)
+      
+      arr[0] = "Original Balance: %0.2f" % [arr[0].to_f]
+      
+      for k in 1..(arr.length()-1)
+         n = arr[k].split(" ")
+         balance -= n[2].to_f 
+         arr[k] = "%s %s %0.2f Balance %0.2f" % [n[0],n[1],n[2].to_f,balance]
       end
-
-      arr.append(format('Total expense  %0.2f', full_balance - balance))
-      arr.append(format('Average expense  %0.2f', (full_balance - balance) / (arr.length - 2)))
-
-      # concate all strings
-      arr.join("\r\n")
+    
+      arr.append("Total expense  %0.2f" % [full_balance-balance])
+      arr.append("Average expense  %0.2f" % [(full_balance-balance)/(arr.length()-2)])  
+      
+      #concate all strings
+      new_b = arr.join("\r\n")
+      
+      return new_b
     end
 
     def self.f(x)
--- kata/Implementation/KmytiukNatalyia/FiveImpl.rb
+++ kata/Implementation/KmytiukNatalyia/FiveImpl.rb
@@ -4,7 +4,7 @@ require './kata/Five'
 
 module KmytiukNatalyiaFiveImpl
   class FiveImpl < Five
-    def gap(g, m, n)
+    def self.gap(g, m, n)
       m += 1 if m.even?
       (m..n).each do |i|
         next unless is_prime(i)
@@ -13,54 +13,55 @@ module KmytiukNatalyiaFiveImpl
       nil
     end
 
-    def is_prime(n)
+    def self.is_prime(n)
       (2..Math.sqrt(n).round).each { |i| return false if (n % i).zero? }
       true
     end
-  end
+  
 
-  def zeros(n)
-    return 0 if n.zero?
+    def self.zeros(n)
+      return 0 if n.zero?
 
-    num = 0
-    while n > 5
-      n /= 5
-      num += n
+      num = 0
+      while n > 5
+        n /= 5
+        num += n
+      end
+      num
     end
-    num
-  end
 
-  def perimeter(n)
-    n1 = 1
-    n2 = 1
-    count = 0
-    sum = 0
-    (1..n + 1).each do |count|
-      sum += n1
-      m = n1 + n2
-      n1 = n2
-      n2 = m
-      count += 1
+    def self.perimeter(n)
+      n1 = 1
+      n2 = 1
+      count = 0
+      sum = 0
+      (1..n + 1).each do |count|
+        sum += n1
+        m = n1 + n2
+        n1 = n2
+        n2 = m
+        count += 1
+      end
+      sum * 4
     end
-    sum * 4
-  end
 
-  def solve(m)
-    return (2 * m + 1 - (4 * m + 1) ** 0.5) / (2 * m)
-    puts solve(8)
-    puts solve(2)
-  end
-
-  def smallest(n)
-    n_arr = n.to_s.split('').map(&:to_i)
-
-    if n_arr.each_index.select { |i| n_arr[i] == n_arr.min }.length == 1
-      n_arr2 = n_arr.dup
-      n_arr2.delete_at(0)
-      ind = n_arr2.each_index.select { |i| n_arr2[i] == n_arr2.min }.max + 1
-    else
-      ind = n_arr.each_index.select { |i| n_arr[i] == n_arr.min }.max
+    def self.solve(m)
+      return (2 * m + 1 - (4 * m + 1) ** 0.5) / (2 * m)
+      puts solve(8)
+      puts solve(2)
     end
-    lowest = n_arr[ind]
+
+    def self.smallest(n)
+      n_arr = n.to_s.split('').map(&:to_i)
+
+      if n_arr.each_index.select { |i| n_arr[i] == n_arr.min }.length == 1
+        n_arr2 = n_arr.dup
+        n_arr2.delete_at(0)
+        ind = n_arr2.each_index.select { |i| n_arr2[i] == n_arr2.min }.max + 1
+      else
+        ind = n_arr.each_index.select { |i| n_arr[i] == n_arr.min }.max
+      end
+      lowest = n_arr[ind]
+    end
   end
 end
