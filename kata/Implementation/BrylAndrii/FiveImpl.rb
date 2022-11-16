6b2d19ca77988dacd333758c7301a2989470f542
Edgers <a.b.23042000@gmail.com>
2022-11-16 23:29:23 +0200

    Insert Commit Message Here

--- kata/Implementation/BrylAndrii/FiveImpl.rb
+++ kata/Implementation/BrylAndrii/FiveImpl.rb
@@ -1,10 +1,18 @@
+# frozen_string_literal: true
+
 require './kata/Five'
 require 'prime'
 
 module BrylAndriiFiveImpl
   class FiveImpl < Five
     # Gap in Primes
-    def self.gap(g_arg, m_arg, n_arg) end
+    def self.gap(g, m, n)
+      (m..n).each do |i|
+        next unless (2..(Math.sqrt(i).to_i)).none? { |n| (i % n).zero? }
+        return [i, i + g] if (2..(Math.sqrt(i + g).to_i)).none? { |n| ((i + g) % n).zero? }
+      end
+      nil
+    end
 
     # Number of trailing zeros of N!
     def self.zeros(num) end
