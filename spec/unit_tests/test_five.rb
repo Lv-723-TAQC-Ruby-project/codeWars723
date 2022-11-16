acd61524b7b4e1e462248fffcbb6b08b17551d94
Edgers <a.b.23042000@gmail.com>
2022-11-16 15:42:44 +0200

    Insert Commit Message Here

--- spec/unit_tests/test_five.rb
+++ spec/unit_tests/test_five.rb
@@ -1,1 +1,38 @@
+require "./utils/ListImpl.rb"
+require 'rspec'
 
+ListImpl::IMPL.each do |student_name, student_impl|
+  student_impl = student_impl::FiveImpl
+
+  RSpec.describe student_impl do
+    describe 'Gap in Primes' do
+
+      it 'Should find the first pair of two prime numbers spaced with a gap of g between the limits m, n if these numbers exist otherwise nil' do
+      end
+    end
+
+    describe 'Trailing zeros in factorial' do
+
+      it 'Should calculate the number of trailing zeros of n!' do
+      end
+    end
+
+    describe 'Perimeter of squares in a rectangle' do
+
+      it 'Should calculate the perimeter of the squares in a rectangle when there are n + 1 squares side by side' do
+      end
+    end
+
+    describe 'Which x for that sum?' do
+
+      it 'Should find all values of x for which the given function is an integer' do
+      end
+    end
+
+    describe 'Find the smallest' do
+
+      it 'Should find the smallest integer n such that n^2 is greater than a given integer p' do
+      end
+    end
+  end
+end
--- spec/unit_tests/test_seven.rb
+++ spec/unit_tests/test_seven.rb
@@ -1,1 +1,27 @@
+require "./utils/ListImpl.rb"
+require 'rspec'
 
+ListImpl::IMPL.each do |student_name, student_impl|
+  student_impl = student_impl::SevenImpl
+
+  RSpec.describe student_impl do
+
+    describe 'Looking for a benefactor' do
+
+      it 'Should calculate how much the benefactor should give to each person such that he/she will have the same amount of money' do
+      end
+    end
+
+    describe 'Sum of the first nth term of Series' do
+
+      it 'Should calculate the sum of the first n terms of the series' do
+      end
+    end
+
+    describe 'Where is Vasya?' do
+
+      it 'Should calculate all the possible positions of Vasya' do
+      end
+    end
+  end
+end
