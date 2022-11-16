95c079f8518a4f008d033ad99f55c486a2a0d601
Edgers <a.b.23042000@gmail.com>
2022-11-16 18:13:21 +0200

    Keep Hydrated test done

--- spec/unit_tests/test_eight.rb
+++ spec/unit_tests/test_eight.rb
@@ -8,7 +8,13 @@ ListImpl::IMPL.each do |student_name, student_impl|
 
     describe 'Keep Hydrated' do
       [
-        [2, 1], [1.4, 0], [12.3, 6], [0.82, 0], [11.8, 5], [1787, 893], [0, 0]
+        [2, 1],
+        [1.4, 0],
+        [12.3, 6],
+        [0.82, 0],
+        [11.8, 5],
+        [1787, 893],
+        [0, 0]
       ].each do |test_data, expect_result|
         it 'Should return the number of litres rounded to the smallest number' do
           expect(student_impl.litres(test_data)).to eq(expect_result)
