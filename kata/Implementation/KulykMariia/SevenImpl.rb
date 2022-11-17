3c5dc3f9278f1e5680ce6210e90da09cef587205
merykul <mariakulv@gmail.com>
2022-11-17 11:21:00 +0300

    added missing tasks

--- kata/Implementation/KulykMariia/SevenImpl.rb
+++ kata/Implementation/KulykMariia/SevenImpl.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
-
+#
 require './kata/Seven'
 
 module KulykMariiaSevenImpl
   class SevenImpl < Seven
+
+    # Looking for a benefactor
     def self.new_avg(arr, newavg)
       result = (arr.length + 1) * newavg - arr.reduce(0) { |sum, n| sum + n }
       result <= 0 ? (raise ArgumentError) : result.ceil
     end
 
+    # Sum of the first nth term of Series
     def self.series_sum(n)
       return '0.00' if n.zero?
 
@@ -21,5 +24,11 @@ module KulykMariiaSevenImpl
       end
       '%.2f' % value
     end
+
+    # Where is Vasya?
+    def self.where_is_he(p, bef, aft)
+      p + 1 - [bef + 1, p - aft].max
+    end
+
   end
 end
