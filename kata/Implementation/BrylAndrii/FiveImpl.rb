55b4be6ca98dfda22cc1b9a755b01e7207bd5a8c
Edgers <a.b.23042000@gmail.com>
2022-11-18 02:56:02 +0200

    Check next commit for message.

--- kata/Implementation/BrylAndrii/FiveImpl.rb
+++ kata/Implementation/BrylAndrii/FiveImpl.rb
@@ -15,15 +15,23 @@ module BrylAndriiFiveImpl
     end
 
     # Number of trailing zeros of N!
-    def self.zeros(num) end
+    def self.zeros(n)
+      n < 5 ? 0 : (n / 5) + zeros(n / 5)
+    end
 
     # Perimeter of squares in a rectangle
-    def self.perimeter(num) end
+    def self.perimeter(n)
+      fib = [1, 1]
+      (n - 1).times do
+        fib << fib[-1] + fib[-2]
+      end
+      fib.inject(:+) * 4
+    end
 
     # Which x for that sum?
-    def self.sum(num) end
+    def self.solve(m) end
 
     # Find the smallest
-    def self.smallest(num) end
+    def self.smallest(n) end
   end
 end
