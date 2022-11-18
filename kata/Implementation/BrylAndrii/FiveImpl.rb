b14d8e0b0e4a4115941aa7bfc044e648797c44b8
Edgers <a.b.23042000@gmail.com>
2022-11-18 02:54:34 +0200

    Check next commit for message.

--- kata/Implementation/BrylAndrii/FiveImpl.rb
+++ kata/Implementation/BrylAndrii/FiveImpl.rb
@@ -1,21 +1,37 @@
+# frozen_string_literal: true
+
 require './kata/Five'
 require 'prime'
 
 module BrylAndriiFiveImpl
   class FiveImpl < Five
     # Gap in Primes
-    def self.gap(g_arg, m_arg, n_arg) end
+    def self.gap(g, m, n)
+      (m..n).each do |i|
+        next unless (2..(Math.sqrt(i).to_i)).none? { |n| (i % n).zero? }
+        return [i, i + g] if (2..(Math.sqrt(i + g).to_i)).none? { |n| ((i + g) % n).zero? }
+      end
+      nil
+    end
 
     # Number of trailing zeros of N!
-    def self.zeros(num) end
+    def self.zeros(n)
+      n < 5 ? 0 : (n / 5) + zeros(n / 5)
+    end
 
     # Perimeter of squares in a rectangle
-    def self.perimeter(num) end
+    def self.perimeter(n)
+      fib = [1, 1]
+      (n - 1).times do
+        fib << fib[-1] + fib[-2]
+      end
+      fib.inject(:+) * 4
+    end
 
     # Which x for that sum?
-    def self.sum(num) end
+    def self.solve(m) end
 
     # Find the smallest
-    def self.smallest(num) end
+    def self.smallest(n) end
   end
 end
