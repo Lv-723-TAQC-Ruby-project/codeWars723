74ee230a760241026036783dbcebc46368cf2098
Edgers <a.b.23042000@gmail.com>
2022-12-04 19:00:39 +0200

    This was the most stupid bug in the world, fixed in the smartest way ever

--- spec/unit_tests/test_seven.rb
+++ spec/unit_tests/test_seven.rb
@@ -7,9 +7,25 @@ ListImpl::IMPL.each do |student_name, student_impl|
   RSpec.describe student_impl do
 
     describe 'Looking for a benefactor' do
-
-      it 'Should calculate how much the benefactor should give to each person such that he/she will have the same amount of money' do
+      [
+        [[14, 30, 5, 7, 9, 11, 16], 90, 628],
+        [[14, 30, 5, 7, 9, 11, 15], 92, 645]
+      ].each do |arr, newavg, expect_result|
+        it 'Should calculate how much the benefactor should give to each person such that he/she will have the same amount of money' do
+          expect(student_impl.new_avg(arr, newavg)).to eq(expect_result)
+        end
       end
+    end
+
+    describe 'Looking For a ArgumentError' do
+      [
+        [[14, 30, 5, 7, 9, 11, 15], 2]
+      ].each do |test_data, expect_result|
+        it 'Should calculate how much the benefactor should give to each person such that he/she will have the same amount of money' do
+          expect{student_impl.new_avg(test_data, expect_result)}.to raise_error(ArgumentError)
+        end
+      end
+
     end
 
     describe 'Sum of the first nth term of Series' do
@@ -17,14 +33,14 @@ ListImpl::IMPL.each do |student_name, student_impl|
         [1, "1.00"],
         [2, "1.25"],
         [3, "1.39"],
-       ].each do |number, expect_result|
-      it 'Should calculate the sum of the first n terms of the series' do
-         expect(student_impl.series_sum(number)).to eq(expect_result)
-       end
+      ].each do |number, expect_result|
+        it 'Should calculate the sum of the first n terms of the series' do
+          expect(student_impl.series_sum(number)).to eq(expect_result)
+        end
       end
     end
 
-     describe 'Where is Vasya?' do
+    describe 'Where is Vasya?' do
       [
         [3, 1, 1, 2],
         [5, 2, 3, 3]
